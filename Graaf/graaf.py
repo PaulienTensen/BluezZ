7496ea3f2379179c4134790a5e0b5337f70b4322
[MattiaSabino] <[Mattiasabinocaso@gmail.com]>
2017-11-07 17:49:33 +0100

    'Doethet'

--- Graaf/graaf.py
+++ Graaf/graaf.py
@@ -42,19 +42,57 @@ for i in range (b):
 
 
 
-dict = {}
+graph = {}
 
 for i in range (b):
     b = {}
     x= stations[i]['Station']
     g=alle_sporen[i]
     b = {x:g}
-    dict.update(b)
-
-print dict
+    graph.update(b)
 
 
-    
+
+
+
+
+def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not graph.has_key(start):
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths		
+		
+			
+
+print(find_all_paths(graph, 'Amsterdam Amstel', 'Beverwijk'))		
+		
+
+
+      
+def find_shortest_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        shortest = None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_shortest_path(graph, node, end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+
+print find_shortest_path(graph, 'Amsterdam Amstel', 'Beverwijk')    
  
  
  
