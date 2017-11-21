0a98f643e4de882fd7c2f9ed9f25bd5810a7ca4a
[MattiaSabino] <[Mattiasabinocaso@gmail.com]>
2017-11-21 16:57:31 +0100

    'classs'

--- Graaf/class.py
+++ Graaf/class.py
@@ -82,15 +82,16 @@ for i in range (b):
     
 # Trein class aanmaken.    
 class Trein(object):
-    def __init__(self, traject, beginstation, tijdsduur):
+    def __init__(self, traject, eindstation, beginstation, tijdsduur):
         self.traject= traject
-        self.beginstation = beginstation
+        self.eindstation = eindstation
         self.tijdsduur = tijdsduur
+        self.beginstation = beginstation
      
     # Vervangt oud station voor huidig station.
     def actuele_station(self,huidig_station):
-        self.beginstation = []
-        self.beginstation.append(huidig_station)
+        self.eindstation = []
+        self.eindstation.append(huidig_station)
       
     # Volgend spoor bepalen. 
     def volgend_spoor(self, nieuw_station):
@@ -100,61 +101,53 @@ class Trein(object):
     def tijd(self, tijd):
         self.tijdsduur += tijd
         
-  
-  
-  
-  
-  
-  
-  
-  
+
     # De verbinding met de kortste tijd kiezen.
     # Hier worden alle beslissingen gemaakt kwa welk spoor te nemen
     # Hier moeten dus de restraints aangepast worden!
     def opties(self, huidig_station):
         richtingen = graph[huidig_station]
-        best_time = int(richtingen[0][1][0])
-        best_station = richtingen[0][0][0]
+        best_time = 10000
         
         for row in richtingen:
-            
-            tijd = int(row[1][0])
-            if tijd <= best_time:
-                best_time = tijd
-                best_station = row[0][0]
+            if row[0][0] not in self.traject:
+                tijd = int(row[1][0])
+                if tijd <= best_time:
+                    best_time = tijd
+                    best_station = row[0][0]
 
         return best_station, best_time
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     def pop(self):
         pop = self.traject.pop()
         
     
     def verminderen(self, laatste_verbinding):
-        thomas.tijdsduur -= laatste_verbinding[1] 
+        self.tijdsduur -= laatste_verbinding[1] 
+        
+        
         
 
-START = ["Den Helder"]    
+START = ["Den Helder"]   
+START2 = ["Rotterdam Alexander"] 
         
 # Trein initialiseren (beginpunt kiezen & tijd op nul zetten).        
 trajectthomas = START   
-thomas = Trein(trajectthomas, START, 0)    
+thomas = Trein(trajectthomas, START, ["Den Helder"], 0)    
+
+
+
+trajectbram = START2
+bram = Trein(trajectbram, START2, ["Rotterdam Alexander"], 0)
+
+
+
+
+
+
+
 
 
 
@@ -165,14 +158,11 @@ thomas = Trein(trajectthomas, START, 0)
 while (thomas.tijdsduur <= 120):
 
     # Beste optie kiezen aan de hand van de mogelijkheden.
-    beste_optie = thomas.opties(thomas.beginstation[0])
-
+    beste_optie = thomas.opties(thomas.eindstation[0])
     # Trein verplaatsen naar volgend spoor.
     thomas.volgend_spoor(beste_optie[0])
-
     # Huiding station updaten.
     thomas.actuele_station(beste_optie[0])
-
     # Tijd updaten.
     thomas.tijd(beste_optie[1])
 
@@ -182,26 +172,71 @@ if thomas.tijdsduur > 120:
     thomas.pop()
     lengte = len(thomas.traject) - 1
     thomas.actuele_station(thomas.traject[lengte])
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# While loop gaat door tot traject is kleiner of gelijk dan 120.
+while (bram.tijdsduur <= 120):
+
+    # Beste optie kiezen aan de hand van de mogelijkheden.
+    beste_optie = bram.opties(bram.eindstation[0])
+    # Trein verplaatsen naar volgend spoor.
+    bram.volgend_spoor(beste_optie[0])
+    # Huiding station updaten.
+    bram.actuele_station(beste_optie[0])
+    # Tijd updaten.
+    bram.tijd(beste_optie[1])
+
+#Al is het traject hoger betekent dat er 1 loop te veel gedaan is, dus die moet eraf gehaald worden.
+if bram.tijdsduur > 120:
+    bram.verminderen(beste_optie)
+    bram.pop()
+    lengte = len(bram.traject) - 1
+    bram.actuele_station(bram.traject[lengte]) 
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
 
 
-
-
-print
-print
-
+    
+    
 
 print
-print "DEEL 2 :"
 print
-
-
+print
+print "THOMAS: :"
+print
 print "traject: ", thomas.traject
 print "tijdsduur: ", thomas.tijdsduur
-print "beginstation: ", thomas.beginstation
-
+print "Beginstation: ", thomas.beginstation
+print "Eindstation: ", thomas.eindstation
+print
+print
+print
+print "BRAM: :"
+print
+print "traject: ", bram.traject
+print "tijdsduur: ", bram.tijdsduur
+print "Beginstation: ", bram.beginstation
+print "Eindstation: ", bram.eindstation
 
 
 
